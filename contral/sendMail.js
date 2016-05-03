fc227090927a6fd9759b5d0b137ae309748a0e85
scc <1437366608@qq.com>
2016-05-03 22:05:23 +0800

    end

--- contral/sendMail.js
+++ contral/sendMail.js
@@ -16,7 +16,7 @@ var sendEmail = function(url,cb) {
         from: email.user,
         to: email.toMail,
         subject: '形式与政策作业',
-        html:  "ppt地址 "+url +' 请尽提前下载'|| '接收失败，请联系腾讯客服'
+        html:  "电子141班 ppt地址 "+url +' 请尽提前下载'|| '接收失败，请联系腾讯客服'
     };
     transports.sendMail(opts, function(err, res) {
         if (err) {
